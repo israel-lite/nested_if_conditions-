b9614876bdb259ee133425e795bbb22757e68eaf
israel-lite <israelite1804@gmail.com>
2025-08-05 21:44:52 +1200

    update: all my updated works

---
+++ work1.py
@@ -0,0 +1,16 @@
+########### version_1
+i = 0
+limit = []
+while i < limit:
+student = {
+	"ID": i + 1,
+	"name": (f"student{i + 1}"),
+	"score": i+1
+	}
+student.append(student)
+i += 1
+
+print("LIST OF 1,000 ODERED STUDENT \n", students)
+
+##########version_2
+
